68a015721cd15533c912785f44792e8d56efbf30
joonisOksanen <99165694+joonisRte66@users.noreply.github.com>
2022-03-01 11:24:09 +0200

    ...

--- rallyRoad1990vs2020/script.js
+++ rallyRoad1990vs2020/script.js
@@ -12,15 +12,3 @@ function removeActiveClasses(){
         panel.classList.remove('active');
     })
 }
-
-let img = document.querySelector('img');
-let btn1 = document.querySelector('#btn1');
-let btn2 = document.querySelector('#btn2');
-
-btn1.addEventListener('click', () => {
-    img.src = 'img/montecarlo_1990';
-})
-
-btn2.addEventListener('click', () => {
-    img.src = 'img/montecarlo_2020';
-})

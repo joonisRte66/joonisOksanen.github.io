cb6ee6db79a3766ab23fd30b06a2a16598b58961
joonisOksanen <99165694+joonisRte66@users.noreply.github.com>
2022-03-02 14:22:12 +0200

    Update script.js

--- rallyRoad1990vs2020/script.js
+++ rallyRoad1990vs2020/script.js
@@ -14,20 +14,20 @@ function removeActiveClasses(){
 }
 function changeone()
 {
+    document.getElementById("div_0").innerHTML='Rally Roads 1990';
+
     document.getElementById("div_1").innerHTML='<img src=img/montecarlo_1990.jpg>';
     document.getElementById("div_2").innerHTML='<img src=img/finland_1990.jpg>';
     document.getElementById("div_3").innerHTML='<img src=img/sweden_1990.jpg>';
     document.getElementById("div_4").innerHTML='<img src=img/estonia_1990.jpg>';
-    
-    document.getElementById("div_0").innerHTML='Rally Roads 1990';
 }
 
 function changetwo()
 {
+    document.getElementById("div_0").innerHTML='Rally Roads 2020';
+
     document.getElementById("div_1").innerHTML='<img src=img/montecarlo_2020.jpg>';
     document.getElementById("div_2").innerHTML='<img src=img/finland_2020.jpg>';
     document.getElementById("div_3").innerHTML='<img src=img/sweden_2020.jpg>';
     document.getElementById("div_4").innerHTML='<img src=img/estonia_2020.jpg>';
-
-    document.getElementById("div_0").innerHTML='Rally Roads 2020';
 }

882d050ee06c85dc4d13b812348cc4293012a4b6
Eemelik4 <99165656+Eemelik4@users.noreply.github.com>
2022-03-17 13:46:04 +0200

    Paranna pizzantilausprosessin dokumentaatiota
    Lisää myös juomille oman alueen.

--- roma_pizza/script.js
+++ roma_pizza/script.js
@@ -1,61 +1,81 @@
-var kinkkuAnanasGluteenitonPohja; // Muuttuja johon tallennetaan tieto tuleeko kinkkuananaspizzaan gluteeniton pohja
-var juustoGluteenitonPohja; // Muuttuja johon tallennetaan tieto tuleeko juustopizzaan gluteention pohja
+/*
+Pizzantilausprosessi.
+Ensimmäinen checkbox-input kerää ensin syötteen onko kyseinen pizza valittu. Toinen input kerää tuotteiden määrän. 
+Lisäksi on 3 muuta checkbox-inputtia jotka keräävät lisätäytteiden tiedot. Jokaisen input IDn on oltava erilainen sillä rakenne käyttää
+'document.getElementById' toimintoa joka hankkii tiedot IDn perusteella. Isot ja pienet kirjaimet myös merkitsevät. Jokainen pizza 
+ruudukko sisältää myös napin 'Lisää ostoskoriin' joka laukaisee funktion PizzanNimiFunktio(). Pizzantilausprosessi ei tue saman pizzan 
+valitsemista useaan kertaan jossa jokaisessa on eri täytteet.
+*/
+
+// Muuttujat johon tallennetaan tiedot tuleeko pizzaan gluteeniton pohja
+var kinkkuAnanasGluteenitonPohja;
+var juustoGluteenitonPohja;
 var operaGluteenitonPohja;
 var kebabGluteenitonPohja;
 var jauhelihaGluteenitonPohja;
 
-var kinkkuPizzaYhteishinta; // Muuttuja johon tallennetaan kinkku pizzojen yhteishinta
-var juustoPizzaYhteishinta; // Muuttuja johon tallennetaan juusto pizzojen yhteishinta
+// Muuttujat johon tallennetaan pizzojen yhteishinnat
+var kinkkuPizzaYhteishinta;
+var juustoPizzaYhteishinta;
 var operaPizzaYhteishinta;
 var kebabPizzaYhteishinta;
 var jauhelihaPizzaYhteishinta;
 
-var onkoJuustoValittu; // Muuttuja johon tallennetaan tieto onko juustopizza valittu
-var onkoKinkkuValittu; // Muuttuja johon tallennetaan tieto onko kinkkupizza valittu
+// Muuttujat johon tallennetaan tiedot onko kyseinen pizza valittu
+var onkoJuustoValittu;
+var onkoKinkkuValittu;
 var onkoOperaValittu;
 var onkoKebabValittu;
 var onkoJauhelihaValittu;
 
-var kaikenYhteisHinta; // Muuttuja johon tallennetaan kaikkien valittujen pizzojen hinta
+// Muuttuja johon tallennetaan kaiken yhteishinta
+var kaikenYhteisHinta;
 
-var juustoLisaKatkarapu; // Muuttuja johon tallennetaan tieto tuleeko juustopizzaan katkarapua
-var juustoLisaSipuli; // Muuttuja johon tallennetaan tieto tuleeko juustopizzaan lisäsipulia
-
+// Muuttujat johon tallennetaan tiedot onko lisätäytteet valittu
+var juustoLisaKatkarapu;
+var juustoLisaSipuli;
 var jauhelihaLisaKatkarapu;
 var jauhelihaLisaSipuli;
-
 var operaLisaKatkarapu;
 var operaLisaSipuli;
-
 var kebabLisaKatkarapu;
 var kebabLisaSipuli;
+var kinkkuAnanasLisaKatkarapu;
+var kinkkuAnanasLisaSipuli;
 
-var kinkkuAnanasLisaKatkarapu; // Muuttuja johon tallennetan tieto tuleeko kinkkuananaspizzaan katkarapua
-var kinkkuAnanasLisaSipuli; // Muttuuja johon tallennetaan tieto tuleeko kinkuananaspizzaan sipulia
+// Muuttujat johon tallennetaan tieto onko kyseinen juoma valittu
+var onkoKokaKolaValittu;
+var onkoJaffaValittu;
+var onkoSpriteValittu;
 
-var onkoKokaKolaValittu; // Muuttuja johon tallennetaan tieto onko cocacola valittu
-var tuleekoKokaKolaJaita; // Muuttuja johon tallennetaan tieto tuleeko cocacolaan jäitä
-var kokaKolaYhteishinta; // Muuttuja johon tallennetaan cocacola juomien yhteishinta
+// Muuttujat johon tallennetaan tieto tuleeko kyseisiin juomiin jäitä
+var tuleekoKokaKolaJaita;
+var tuleekoJaffaJaita;
+var tuleekoSpriteJaita;
 
-var onkoJaffaValittu; // Muuttuja johon tallennetaan tieto onko jaffa valittu
-var tuleekoJaffaJaita; // Muuttuja johon tallennetaan tieto tuleeko jaffaan jäitä
-var jaffaYhteishinta; // Muuttuja johon tallennetaan jaffojen yhteishinta
+// Muuttujat johon tallennetaan jokaisen juoman yhteishinta
+var kokaKolaYhteishinta;
+var jaffaYhteishinta;
+var spriteYhteishinta;
 
-var onkoSpriteValittu; // Muuttuja johon tallennetaan tieto onko sprite valittu
-var tuleekoSpriteJaita; // Muuttuja johon tallennetaan tieto tuleeko spriteen jäitä
-var spriteYhteishinta // Muuttuja johon tallennetaan sprite juomien yhteishinta
-
-// Funktio jolla lasketaan kinkkuananaspizzan määrä ja hinta
+/*
+Jokaisella pizzalla on oma funktio joka käynnistyy 'Lisää ostoskoriin' painamisella. Funktio luo ja alustaa muuttujan johon tallennetaan kyseisten pizzojen määrä.
+Funktio myös tallentaa muuttujaan tiedon onko kyseinen pizza valittu. Funktio tarkistaa onko kyseinen pizza valittu, jos ei ole
+mutta käyttäjä yrittää silti tilata sen, tulostaa tämä virhe viestin ja tyhjentää muut mahdolliset tekstit. Funktio myös
+tarkistaa että pizzoja on valittu enemmän kuin 0, ja jos ei niin tulostaa tämä toisen virheviestin. Jos tarkastukset menevät läpi,
+tallennetaan pizzan yhteishinnaksi Määrä * Hinta ja kutsutaan funktioita PizzanNimiLisaTayte(), tyhjennetään virheviesti ja kutsutaan
+toista funktiota laskeYhteisHinta() joka päivittää yhteishinnan aina automaattisesti.
+*/
 function kinkkuFunktio() {
   var kinkkuAnanasMaara = 0; // Muuttuja johon tallennetaan kinkkuananaspizzojen määrä
   onkoKinkkuValittu = document.getElementById("kinkkuananas"); // Tallennetaan tieto onko kinkkuananas valittu muuttujaan onkoKinkkuValittu
   kinkkuAnanasMaara = document.getElementById("kinkkuAnanasMaara"); // Tallennetaan kinkkuananaspizzojen määrä muuttujaan
-  if (onkoKinkkuValittu.checked) { // Tarkistetaan onko kinkkuananaspizzaa valittu
+  if (onkoKinkkuValittu.checked) { //Tarkistetaan onko kinkkuananaspizzaa valittu
     if (kinkkuAnanasMaara.value > 0) { // Tarkistaa onko kinkkuananas pizzoja enemmän kuin 0
       kinkkuPizzaYhteishinta = kinkkuAnanasMaara.value * 8; // Kinkkuananaspizzojen yhteishinta = Määrä * Hinta (8e)
       kinkkuAnanasLisaTayte() // Kutsuu funktiota
       document.getElementById("kinkkuVirhe").innerHTML = ""; // Jos pizzoja on yli 0 ja rasti ruudussa niin virhe teksti poistetaan
-      laskeYhteisHinta()
+      laskeYhteisHinta() // Kutsuu funktiota
     } else { // Jos kinkkuananaspizzoja on valittu alle 0
       document.getElementById("kinkkuVirhe").innerHTML = "VIRHE: Valitse vähintään yksi";
       document.getElementById("kinkkuAnanasLisaTayte").innerHTML = "";
@@ -67,7 +87,6 @@ function kinkkuFunktio() {
     document.getElementById("valitutKinkkuPizzat").innerHTML = "";
   }
 }
-
 function juustoFunktio() {
   var juustoPizzaMaara = 0;
   onkoJuustoValittu = document.getElementById("juustoPizza");
@@ -89,7 +108,6 @@ function juustoFunktio() {
     document.getElementById("valitutJuustoPizzat").innerHTML = "";
   }
 }
-
 function operaFunktio() {
   var operaPizzaMaara = 0;
   onkoOperaValittu = document.getElementById("operaPizza");
@@ -111,7 +129,6 @@ function operaFunktio() {
     document.getElementById("valitutOperaPizzat").innerHTML = "";
   }
 }
-
 function kebabFunktio() {
   var kebabPizzaMaara = 0;
   onkoKebabValittu = document.getElementById("kebabPizza");
@@ -133,7 +150,6 @@ function kebabFunktio() {
     document.getElementById("valitutKebabPizzat").innerHTML = "";
   }
 }
-
 function jauhelihaFunktio() {
   var jauhelihaPizzaMaara = 0;
   onkoJauhelihaValittu = document.getElementById("jauhelihaPizza");
@@ -155,7 +171,6 @@ function jauhelihaFunktio() {
     document.getElementById("valitutJauhelihaPizzat").innerHTML = "";
   }
 }
-
 function pepperoniFunktio() {
   var pepperoniPizzaMaara = 0;
   onkoPepperoniValittu = document.getElementById("pepperoniPizza");
@@ -178,6 +193,11 @@ function pepperoniFunktio() {
   }
 }
 
+/*
+Jokaisella juomalla on myös oma funktio. Funktio luo muuttujan johon se tallentaa kyseisen juoman määrän. Se myös kerää tiedot onko kyseinen juoma ylipäätään valittu.
+Funktio tarkistaa onko juoma valittu ja että niiden määrä on yli 0. Jos ei niin luo tämä virhe viestin. Jos tarkastukset menevät läpi, tulee juoman yhteishinnaksi
+Määrä * Hinta. Se myös kutsuu funktiota juomaLisaJaat() ja laskeYhteisHinta().
+*/
 function cocaColaFunktio() {
   var cocaColaJuomaMaara = 0;
   onkoKokaKolaValittu = document.getElementById("cocaCola");
@@ -202,12 +222,10 @@ function cocaColaFunktio() {
     document.getElementById("valitutCocaColat").innerHTML = "";
   }
 }
-
 function spriteFunktio() {
   var spriteJuomaMaara = 0;
   onkoSpriteValittu = document.getElementById("sprite");
   spriteJuomaMaara = document.getElementById("spriteJuomaMaara");
-
   if (onkoSpriteValittu.checked) {
     if (spriteJuomaMaara.value > 0) {
       spriteYhteishinta = spriteJuomaMaara.value * 3;
@@ -225,12 +243,10 @@ function spriteFunktio() {
     document.getElementById("valitutSpritet").innerHTML = "";
   }
 }
-
 function jaffaFunktio() {
   var jaffaJuomaMaara = 0;
   onkoJaffaValittu = document.getElementById("jaffa");
   jaffaJuomaMaara = document.getElementById("jaffaJuomaMaara");
-
   if (onkoJaffaValittu.checked) {
     if (jaffaJuomaMaara.value > 0) {
       jaffaYhteishinta = jaffaJuomaMaara.value * 3;
@@ -248,6 +264,11 @@ function jaffaFunktio() {
     document.getElementById("valitutJaffat").innerHTML = "";
   }
 }
+
+/*
+pizzanNimiLisaTayte() funktio kerää tiedot onko lisätäytteitä valittu. Funktio käynnistyy pizzanNimifunktion lopussa. Se myös kerää tiedon
+onko gluteeniton pohja valittu ja tulostaa tekstin "Lisätty!".
+*/
 function kinkkuAnanasLisaTayte() {
   kinkkuAnanasLisaKatkarapu = document.getElementById("lisaKatkarapu"); // Lukee inputin kyseisiin muuttujiin
   kinkkuAnanasLisaSipuli = document.getElementById("lisaSipuli");
@@ -285,7 +306,7 @@ function pepperoniLisaTayte() {
   document.getElementById("pepperoniLisaTayte").innerHTML = "Lisätty!";
 }
 
-
+// juomanNimiLisaJaat() funktio kerää tiedot tuleeko juomaan lisäjäitä. Funktio käynnistyy juomanNimifunktion lopussa.
 function cocaColaLisaJaat() {
   tuleekoKokaKolaJaita = document.getElementById("cocaColaLisaJaa");
   document.getElementById("cocaColaLisaJaat").innerHTML = "Lisätty!";
@@ -298,7 +319,13 @@ function jaffaLisaJaat() {
   tuleekoJaffaJaita = document.getElementById("jaffaLisaJaa");
   document.getElementById("jaffaLisaJaat").innerHTML = "Lisätty!";
 }
-// Funktio jolla lasketaan valittujen pizzojen yhteishinta
+
+
+/*
+Funktio laskeYhteisHinta() käynnistyy joka kerta kun tuote lisätään ostoskoriin. Se lisää yhteishintaan pizzojen määrän * hinnan. Voisi tulevaisuudessa olla vain
++ tuotteenyhteishinnat. Funktio myös tarkistaa onko lisätäytteitä / gluteeniton pohja valittu ja lisää näiden summat yhteishintaan. Tämän jälkeen funktio
+tulostaa yhteishinnan ja valitut tuotteet. Funktiota voisi muuntaa selkeämmäksi mutta se ei ole prioriteetti juuri nyt.
+*/
 function laskeYhteisHinta() {
   kaikenYhteisHinta = kinkkuAnanasMaara.value * 8 + juustoPizzaMaara.value * 5 + operaPizzaMaara.value * 13 + kebabPizzaMaara.value * 7 + jauhelihaPizzaMaara.value * 6 + cocaColaJuomaMaara.value * 3 + spriteJuomaMaara.value * 3 + jaffaJuomaMaara.value * 3 + pepperoniPizzaMaara.value * 8;
   if (kinkkuAnanasMaara.value > 0) { // Jos kinkkuananas pizzoja on enemmän kuin 0 niin jatketaan:
@@ -334,7 +361,6 @@ function laskeYhteisHinta() {
       kaikenYhteisHinta = kaikenYhteisHinta + 2 * operaPizzaMaara.value;
     }
   }
-
   if (kebabPizzaMaara.value > 0) {
     if (kebabLisaKatkarapu.checked) {
       kaikenYhteisHinta = kaikenYhteisHinta + 1 * kebabPizzaMaara.value;
@@ -346,7 +372,6 @@ function laskeYhteisHinta() {
       kaikenYhteisHinta = kaikenYhteisHinta + 2 * kebabPizzaMaara.value;
     }
   }
-
   if (jauhelihaPizzaMaara.value > 0) {
     if (jauhelihaLisaKatkarapu.checked) {
       kaikenYhteisHinta = kaikenYhteisHinta + 1 * jauhelihaPizzaMaara.value;
@@ -358,7 +383,6 @@ function laskeYhteisHinta() {
       kaikenYhteisHinta = kaikenYhteisHinta + 2 * jauhelihaPizzaMaara.value;
     }
   }
-
   if (pepperoniPizzaMaara.value > 0) {
     if (pepperoniLisaKatkarapu.checked) {
       kaikenYhteisHinta = kaikenYhteisHinta + 1 * pepperoniPizzaMaara.value;
@@ -370,9 +394,7 @@ function laskeYhteisHinta() {
       kaikenYhteisHinta = kaikenYhteisHinta + 2 * pepperoniPizzaMaara.value;
     }
   }
-
   document.getElementById("hinta").innerHTML = "Yhteishinta: " + kaikenYhteisHinta + " €"; // Tulostaa yhteishinnan
-
   if (kinkkuAnanasMaara.value > 0) { // Jos kinkkuananas pizzoja on enemmän kuin 0 niin jatketaan:
     if (lisaKatkarapu.checked || lisaSipuli.checked) {  // Jos kinkkuananas pizzoissa on lisätäytteitä niin jatketaan:
       document.getElementById("valitutKinkkuPizzat").innerHTML = kinkkuAnanasMaara.value + " kinkkuananaspizzaa valituilla lisätäytteillä";
@@ -400,7 +422,6 @@ function laskeYhteisHinta() {
       document.getElementById("valitutOperaPizzat").innerHTML = operaPizzaMaara.value + " operapizzaa"
     }
   }
-
   if (kebabPizzaMaara.value > 0) {
     if (kebabLisaKatkarapu.checked || kebabLisaSipuli.checked) {
       document.getElementById("valitutKebabPizzat").innerHTML = kebabPizzaMaara.value + " kebabpizzaa valituilla lisätäytteillä";
@@ -410,7 +431,6 @@ function laskeYhteisHinta() {
       document.getElementById("valitutKebabPizzat").innerHTML = kebabPizzaMaara.value + " kebabpizzaa"
     }
   }
-
   if (jauhelihaPizzaMaara.value > 0) {
     if (jauhelihaLisaKatkarapu.checked || jauhelihaLisaSipuli.checked) {
       document.getElementById("valitutJauhelihaPizzat").innerHTML = jauhelihaPizzaMaara.value + " jauhelihapizzaa valituilla lisätäytteillä";
@@ -430,8 +450,6 @@ function laskeYhteisHinta() {
     }
   }
 
-
-
   if (cocaColaJuomaMaara.value > 0) {
     if (tuleekoKokaKolaJaita.checked) {
       document.getElementById("valitutCocaColat").innerHTML = cocaColaJuomaMaara.value + " Cocacolaa jäillä";
@@ -439,7 +457,6 @@ function laskeYhteisHinta() {
       document.getElementById("valitutCocaColat").innerHTML = cocaColaJuomaMaara.value + " Cocacolaa ilman jäitä";
     }
   }
-
   if (spriteJuomaMaara.value > 0) {
     if (tuleekoSpriteJaita.checked) {
       document.getElementById("valitutSpritet").innerHTML = spriteJuomaMaara.value + " Spriteä jäillä";
@@ -447,7 +464,6 @@ function laskeYhteisHinta() {
       document.getElementById("valitutSpritet").innerHTML = spriteJuomaMaara.value + " Spriteä ilman jäitä";
     }
   }
-
   if (jaffaJuomaMaara.value > 0) {
     if (tuleekoJaffaJaita.checked) {
       document.getElementById("valitutJaffat").innerHTML = jaffaJuomaMaara.value + " Jaffaa jäillä";
@@ -455,8 +471,6 @@ function laskeYhteisHinta() {
       document.getElementById("valitutJaffat").innerHTML = jaffaJuomaMaara.value + " Jaffaa ilman jäitä";
     }
   }
-
-
 }
 // 2 funktiota jotka toteutuu kun käyttäjä valitsee joko 'nouto' tai 'kuljetus'
 function kotiinKuljetus() { // Jos käyttäjä valitsee kuljetuksen
@@ -467,6 +481,8 @@ function tilausNouto() { // Jos käyttäjä valitsee noudon
   document.getElementById("kuljetus").innerHTML = kaikenYhteisHinta * 0.5 + " min valmistumiseen";
   document.getElementById("huvaksu").style.display = "none"; // Piiloittaa osoitekentän jos noutotilaus valitaan
 }
+// PIZZANTILAUSPROSESSI LOPPUU TÄHÄN!
+
 
 function myOrderF(){
   location.href = "orderF.html";

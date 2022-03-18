bba926940b0e597cba24499f7a01ebe1ac52d896
joonisOksanen <99165694+joonisRte66@users.noreply.github.com>
2022-03-18 14:04:05 +0200

    Update script.js

--- roma_pizza/script.js
+++ roma_pizza/script.js
@@ -500,20 +500,3 @@ function myFeed(){
   location.href = "index.html";
   alert("Kiitos palautteesta.");
 }
-
-function ValidateEmail(inputText)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
-}

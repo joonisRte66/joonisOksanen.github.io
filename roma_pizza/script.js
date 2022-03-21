0fa9fd4f4317ab7a634c44594c0d1c981b3b99c6
joonisOksanen <joonis.oksanen@edu.keuda.fi>
2022-03-21 09:19:00 +0200

    Login ja registeröinti done

--- roma_pizza/script.js
+++ roma_pizza/script.js
@@ -529,3 +529,55 @@ function validate(){
   alert("Lähetetty onnistuneesti!");
   return true;
 }
+
+function store(){
+
+  var name = document.getElementById('name');
+  var pw = document.getElementById('pw');
+  var lowerCaseLetters = /[a-z]/g;
+  var upperCaseLetters = /[A-Z]/g;
+  var numbers = /[0-9]/g;
+
+  if(name.value.length == 0){
+      alert('Täytä sähköpostisi!');
+
+  }else if(pw.value.length == 0){
+      alert('Täytä salasana!');
+
+  }else if(name.value.length == 0 && pw.value.length == 0){
+      alert('Täytä sähköpostisi ja salasanasi!');
+
+  }else if(pw.value.length > 8){
+      alert('Maksimissaan 8');
+
+  }else if(!pw.value.match(numbers)){
+      alert('Lisää yksi numero!');
+
+  }else if(!pw.value.match(upperCaseLetters)){
+      alert('Lisää 1 iso kirjain!');
+
+  }else if(!pw.value.match(lowerCaseLetters)){
+      alert('Lisää yksi pieni kirjain!');
+
+  }else{
+      localStorage.setItem('name', name.value);
+      localStorage.setItem('pw', pw.value);
+      alert('Olet rekisteröitynyt!');
+  }
+}
+
+//checking
+function check(){
+  var storedName = localStorage.getItem('name');
+  var storedPw = localStorage.getItem('pw');
+
+  var userName = document.getElementById('userName');
+  var userPw = document.getElementById('userPw');
+  var userRemember = document.getElementById("rememberMe");
+
+  if(userName.value == storedName && userPw.value == storedPw){
+      alert('Olet kirjautunut sisään.');
+  }else{
+      alert('Virhe kirjautumisessa.');
+  }
+}

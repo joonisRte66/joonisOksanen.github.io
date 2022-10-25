57148b8834827c27c792bff7632e4f6b5e8cdb61
joonisOksanen <99165694+joonisRte66@users.noreply.github.com>
2022-10-25 14:05:40 +0300

    Add files via upload

---
+++ discordBot/script.js
@@ -0,0 +1,40 @@
+whurl="https://discord.com/api/webhooks/1034418803574050827/5FyjlWWVxUW6J-MZjQq1mPPj8iLHotKA0GGgsYdiPi_3w5OGudx_Pou7bzsm-qJ2amYL"
+var str= "";
+var name= "";
+function f1(){
+    name = document.getElementById("NameInput").value;
+    str = document.getElementById("InputField").value;
+    console.log(document.getElementById("InputField").value)
+}
+function send(){
+    f1();
+    const msg = {
+        "content": str,
+        "username": name
+    };
+    console.log(msg)
+    if(str == ""){
+        document.getElementById("Message1").style.opacity = 1; 
+        setTimeout(function(){
+            document.getElementById("Message1").style.opacity = 0;
+        }, 4000)
+        console.log("ERROR")
+        return;
+    }
+    try{
+        fetch(whurl + "?wait=true", {"method":"POST", "headers": {"content-type": "application/json"}, "body": JSON.stringify(msg)});
+        document.getElementById("InputField").value = "";
+        document.getElementById("MessageSent").style.opacity = 1;
+        setTimeout(function(){
+            document.getElementById("MessageSent").style.opacity = 0;
+        }, 4000)
+
+    } catch(e){
+        document.getElementById("MessageFailed").style.opacity = 1;  
+        
+        setTimeout(function(){
+            document.getElementById("MessageFailed").style.opacity = 0;
+        }, 4000)
+    }
+
+} 
